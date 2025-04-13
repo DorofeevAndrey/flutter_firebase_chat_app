929f5066b30e2963eb1b3a11ccc2b4ccd00bdea2
Andrey <dorofeevandrey78@gmail.com>
2025-04-14 00:10:58 +0500

    settings, home page and my drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_chat_app/firebase_options.dart';
 
-import 'auth/login_or_register.dart';
+import 'auth/auth_gate.dart';
 import 'themes/light_mode.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
@@ -15,7 +20,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: lightMode,
       debugShowCheckedModeBanner: false,
-      home: LoginOrRegister(),
+      home: const AuthGate(),
     );
   }
 }
