8dc18aafd9f0fa006e78a7a193baae5b76f2616d
Andrey <dorofeevandrey78@gmail.com>
2025-04-13 12:50:09 +0500

    login_page

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+import '../components/my_button.dart';
+import '../components/my_textfield.dart';
+
+class LoginPage extends StatelessWidget {
+  //email and pw text controllers
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _pwController = TextEditingController();
+
+  LoginPage({super.key});
+
+  // login
+  void login() {
+    //
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // logo
+            Icon(Icons.message, size: 60, color: theme.colorScheme.primary),
+            const SizedBox(height: 50),
+
+            // welcome back message
+            Text(
+              "Welcome back, you've been missed!",
+              style: TextStyle(color: theme.colorScheme.primary, fontSize: 16),
+            ),
+            const SizedBox(height: 25),
+
+            // email textfield
+            MyTextfield(
+              hintText: "Email",
+              obscureText: false,
+              controller: _emailController,
+            ),
+            const SizedBox(height: 10),
+
+            // pw textfield
+            MyTextfield(
+              hintText: "Password",
+              obscureText: true,
+              controller: _pwController,
+            ),
+            const SizedBox(height: 25),
+
+            // login button
+            MyButton(title: "Login", onTap: login),
+            const SizedBox(height: 25),
+
+            // regiter now
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Not remember? ",
+                  style: TextStyle(color: theme.colorScheme.primary),
+                ),
+                Text(
+                  "Register now",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: theme.colorScheme.primary,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
